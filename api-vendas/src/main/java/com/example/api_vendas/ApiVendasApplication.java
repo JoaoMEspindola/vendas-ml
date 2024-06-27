28baec55a26248d7891708c4028edba2fb6aaca2
Joao <jpmaresp@gmail.com>
2024-06-27 15:47:25 -0300

    Added MySqlDB; View refactored

--- api-vendas/src/main/java/com/example/api_vendas/ApiVendasApplication.java
+++ api-vendas/src/main/java/com/example/api_vendas/ApiVendasApplication.java
@@ -14,19 +14,4 @@ public class ApiVendasApplication {
 		SpringApplication.run(ApiVendasApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(ProductRepository productRepository){
-		return agrs -> {
-			productRepository.deleteAll();
-			Product product = new Product();
-			product.setName("joao");
-			product.setReference("1234");
-			product.setAmount(12);
-			product.setBrand("oioi");
-			product.setImage("https://lojaacal.vteximg.com.br/arquivos/ids/166013-1000-1000/conjunto-de-tomada-simples-10a-branco-pial-legrand-376518.jpg?v=637389008899630000");
-
-			productRepository.save(product);
-		};
-	}
-
 }
